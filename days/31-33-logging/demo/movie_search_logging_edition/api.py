339630653fe728e262191e15169507a970d0af5b
Matthew Anderson <matt.anderson.inf@gmail.com>
2018-10-23 22:36:50 -0400

    increase my understanding of each of the parts of the api of the movie search application

--- days/31-33-logging/demo/movie_search_logging_edition/api.py
+++ days/31-33-logging/demo/movie_search_logging_edition/api.py
@@ -10,25 +10,33 @@ import time
 Movie = collections.namedtuple('Movie', 'imdb_code, title, director, keywords, '
                                         'duration, genres, rating, year, imdb_score')
 
+# log information as the application runs
 api_log = logbook.Logger('API')
 
-
+# given a string, search the the html of the talkpython movie search site
 def find_movie_by_title(keyword: str) -> List[Movie]:
+    # initialize an object to keep track the run time
     t0 = time.time()
 
     api_log.trace('Starting search for {}'.format(keyword))
 
+    # fail safe in case this function is not given a string object and report it into the logger object
     if not keyword or not keyword.strip():
         api_log.warn("No keyword supplied")
         raise ValueError('Must specify a search term.')
 
+    # move search url, it's here because there's no point in generating it any sooner and it's only tailored to this website
     url = f'http://movie_service.talkpython.fm/api/search/{keyword}'
 
+    # create requests object
     resp = requests.get(url)
     api_log.trace("Request finished, status code {}.".format(resp.status_code))
     resp.raise_for_status()
 
+    # the site's html is designed in such a way that simply merging the base url with the searched word, it will return a set of results that can be formatted into json format
+    # this is not universal as proven through web scraping experiences
     results = resp.json()
+    # call on the create random error method
     results = create_random_errors(results)
 
     movies = []
